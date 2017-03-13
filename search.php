83081e84d023d76373c70f6b38532234fcde0d21
Matt Garner <matthewhgarner@gmail.com>
2017-03-12 21:22:08 -0400

    Update search.php

--- search.php
+++ search.php
@@ -6,8 +6,12 @@
     <h2>Search Events By Tag</h2>
     <form method="POST" action="search.php">
       <fieldset>
-      <legend>Search for specific event by tag</legend>
-      <label for="tagsearch">Tag: </label>
+      <legend>Search for specific event by:</legend>
+			<input type="radio" name="Search" value="Charity" checked> Charity 
+			<input type="radio" name="Search" value="Tag"> Tag 
+			<input type="radio" name="Search" value="Event"> Event 
+			<br>
+			<label for="tagsearch">Search: </label>
       <input type="text" name="tagsearch"><br>
       <input type="submit" value="Submit">
       </fieldset>
@@ -16,16 +20,39 @@
   
   
   <?php
-	$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "xxx", "xxx", "xxx");
-
+	$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "db-name", "password", "db-name");
+	
+	$charSearch = "";
+	
 	if (!$mysqli || $mysqli->connect_errno) { print "No connection: " . $mysqli->connect_errno . " " . $mysqli->connect_error; } 
-
 	// Query the search item
-	$charSearch  = "SELECT charity.name, event.name, tag.name FROM event INNER JOIN
+	if (isset($_POST['Search']) && $_POST['Search'] == "Tag")
+	{
+	$charSearch = "SELECT charity.name, event.name, tag.name, task.name FROM event INNER JOIN
 					charity ON event.c_id = charity.id INNER JOIN
 					charity_tag ON charity_tag.c_id = charity.id INNER JOIN
-					tag ON charity_tag.t_id = tag.id WHERE tag.name REGEXP ?;";
-	
+					tag ON charity_tag.t_id = tag.id INNER JOIN
+					task ON event.id = task.e_id
+					WHERE tag.name REGEXP ?;";
+	}
+	else if (isset($_POST['Search']) && $_POST['Search'] == "Charity")
+	{
+	$charSearch = "SELECT charity.name, event.name, tag.name, task.name FROM event INNER JOIN
+					charity ON event.c_id = charity.id INNER JOIN
+					charity_tag ON charity_tag.c_id = charity.id INNER JOIN
+					tag ON charity_tag.t_id = tag.id INNER JOIN
+					task ON event.id = task.e_id
+					WHERE charity.name REGEXP ?;";
+	}
+	else
+	{
+	$charSearch = "SELECT charity.name, event.name, tag.name, task.name FROM event INNER JOIN
+					charity ON event.c_id = charity.id INNER JOIN
+					charity_tag ON charity_tag.c_id = charity.id INNER JOIN
+					tag ON charity_tag.t_id = tag.id INNER JOIN
+					task ON event.id = task.e_id
+					WHERE event.name REGEXP ?;";
+	}
 	if (isset($_POST['tagsearch']) && $_POST['tagsearch'] != "") {		
 		$tagsrch = $mysqli->prepare($charSearch);
 	
@@ -40,17 +67,17 @@
 			print "Charity/tag search execute failed: " . $tagsrch->errno . " " . $tagsrch->error;
 		}
 	
-		if (!($tagsrch->bind_result($cname, $ename, $tname))) {
+		if (!($tagsrch->bind_result($cname, $ename, $tname, $taname))) {
 			print "Charity/tag search bind result failed: " . $tagsrch->errno . " " . $tagsrch->error;
 		}
-		print "<p>Here are the charities we found: </p>";
+		print "<p>Here are the events we found: </p>";
 		
 		// Display the results of the search in a table
-		$foundCharities = "<table id='charitiesTable'><thead><th>Charity Name<th>Event Name<th>Tag<tbody>";
+		$foundCharities = "<table id='charitiesTable'><thead><th>Charity Name<th>Event Name<th>Tag<th>Tasks<tbody>";
 		
 		while ($tagsrch->fetch()) {
 			// Each found charity will show up in a table row
-			$foundCharities .= "<tr><td>" . $cname . "<td>" . $ename . "<td>" . $tname;
+			$foundCharities .= "<tr><td>" . $cname . "<td>" . $ename . "<td>" . $tname . "<td>" . $taname;
 		}
 		
 		$foundCharities .= "</tbody></table><br>";
