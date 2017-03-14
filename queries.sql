50da14c2f96374082130716967e70a55c2669b31
Matt Garner <matthewhgarner@gmail.com>
2017-03-14 18:56:32 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -13,49 +13,35 @@ INSERT INTO `charity` (`name`,`founder`,`year_founded`, `email`, `password`) VAL
 ("Helping Hands", "Mary Shields", 2001,"test2@email.com", "password"), 
 ("Food for the Hungry", "Robert Jones", 2013,"test3@email.com", "password");
 
-SELECT * from `charity`;
-
 INSERT INTO `event` (`c_id`, `name`, `information`, `time`) VALUES 
 ((SELECT id FROM `charity` WHERE name="testname"), "Fundraiser", "raise funds", "March 5th, noon"), 
 ((SELECT id FROM `charity` WHERE name="Helping Hands"), "pancake breakfast", "free pancake breakfast", "March 12th, 8AM"), 
 ((SELECT id FROM `charity` WHERE name="Food for the Hungry"), "free dinner", "free dinner at 5", "March 21st, 5PM");
-
-SELECT * from `event`;
 
 INSERT INTO `task` (`e_id`, `name`, `description`) VALUES 
 ((SELECT id FROM `event` WHERE name="Fundraiser"), "taskname", "task description"), 
 ((SELECT id FROM `event` WHERE name="pancake breakfast"), "make pancakes", "cook and serve pancakes"), 
 ((SELECT id FROM `event` WHERE name="free dinner"), "bring ingredients", "bring ingredients for the meal");
 
-SELECT * from `task`;
-
-INSERT INTO `volunteer` (`name`, `email`) VALUES 
-("volunteer name", "volunteer@email.com"), 
-("Betsy Smith", "bs4700@gmail.com"), 
-("Bob Stanley", "bob46325@hotmail.com");
-
-SELECT * from `volunteer`;
+INSERT INTO `volunteer` (`name`, `email`, `password`) VALUES 
+("volunteer name", "volunteer@email.com", "superman1"), 
+("Betsy Smith", "bs4700@gmail.com", "password"), 
+("Bob Stanley", "bob46325@hotmail.com", "ilovecats");
 
 INSERT INTO `tag` (`name`) VALUES 
 ("tag Name"), 
 ("Hunger"), 
 ("Homelessness");
 
-SELECT * from `tag`;
-
 INSERT INTO `volunteer_task` (`v_id`, `t_id`) VALUES 
 ((SELECT `id` FROM `volunteer` WHERE email="volunteer@email.com"), (SELECT `id` FROM `task` WHERE name="taskname")),
 ((SELECT `id` FROM `volunteer` WHERE email="bs4700@gmail.com"), (SELECT `id` FROM `task` WHERE name="make pancakes")),
 ((SELECT `id` FROM `volunteer` WHERE email="bob46325@hotmail.com"), (SELECT `id` FROM `task` WHERE name="bring ingredients"));
 
-SELECT * FROM `volunteer_task`;
-
 INSERT INTO `charity_tag` (`c_id`, `t_id`) VALUES 
 ((SELECT `id` FROM `charity` WHERE name="testname"), (SELECT `id` FROM `tag` WHERE name="tag Name")), 
 ((SELECT `id` FROM `charity` WHERE name="Food for the Hungry"), (SELECT `id` FROM `tag` WHERE name="Hunger")), 
 ((SELECT `id` FROM `charity` WHERE name="Helping Hands"), (SELECT `id` FROM `tag` WHERE name="Homelessness"));
-
-SELECT * FROM `charity_tag`;
 
 SELECT task.name FROM charity INNER JOIN
 event ON event.c_id = charity.id INNER JOIN
@@ -71,4 +57,16 @@ charity ON event.c_id = charity.id INNER JOIN
 charity_tag ON charity_tag.c_id = charity.id INNER JOIN
 tag ON charity_tag.t_id = tag.id;
 
+SELECT * from `charity`;
 
+SELECT * from `event`;
+
+SELECT * from `task`;
+
+SELECT * from `volunteer`;
+
+SELECT * from `tag`;
+
+SELECT * FROM `volunteer_task`;
+
+SELECT * FROM `charity_tag`;
